80faf50462c1797d6965269cd80dfe57fbedc2c7
Undeadzone <nikolai_aramazov@abv.bg>
2026-01-14 12:03:54 +0200

    .

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -118,8 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.environ.get("STATIC_ROOT", BASE_DIR / "static")
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = Path("/home/site/wwwroot/static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -127,4 +126,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.environ.get("MEDIA_ROOT", BASE_DIR / "media")
+MEDIA_ROOT = Path("/home/site/wwwroot/media")
